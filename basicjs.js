b85faf5a9f9458b4d39a45c9245ac50bdf3d7154
calamanshi <calamanshi@gmx.at>
2019-09-24 11:11:04 +0200

    yolo

--- basicjs.js
+++ basicjs.js
@@ -1,25 +1,36 @@
-var temperature =55;
+// var temperature =55;
 
-if (temperature <= 10){
-	console.log ("fresh in here");
+// if (temperature <= 10){
+// 	console.log ("fresh in here");
+// }
+// else {
+// 	console.log ("not so fresh");
+// }
+// // temperature = 5;
+
+// var highVal = [1,7,-3,9];
+
+// document.write (Math.max.apply(undefined, highVal));
+// document.write (Math.max.apply(null, highVal));
+// document.write (Math.max.apply(Math, highVal));
+
+// var multis = [1,3,7,10,2];
+// function multiplication (a) {
+// 	multi = a[0];
+// 	for (i=1; i<parseInt(a.length); i++) {
+// 		multi = multi * a[i];
+// 	}
+// }
+// 	multiplication(multis);
+// 	console.log(multi);
+
+var usertemp = prompt("enter the hotness");
+console.log (usertemp);
+
+if (parseInt(usertemp)<25){
+	document.body.style.backgroundColor = "blue";
 }
 else {
-	console.log ("not so fresh");
+	document.body.style.backgroundColor = "salmon";
 }
-// temperature = 5;
 
-var highVal = [1,7,-3,9];
-
-document.write (Math.max.apply(undefined, highVal));
-document.write (Math.max.apply(null, highVal));
-document.write (Math.max.apply(Math, highVal));
-
-var multis = [1,3,7,10,2];
-function multiplication (a) {
-	for (i=0; i<a.length; i++) {
-		b = a[i];
-		b =parseInt(b)*parseInt(a[i+1]);
-		return b;
-	}}
-	multiplication(multis);
-	console.log(b);
