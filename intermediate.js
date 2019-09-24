76258a1ef290646df8f580aefb3e3d8676f845c8
calamanshi <calamanshi@gmx.at>
2019-09-24 13:55:56 +0200

    yolo

--- intermediate.js
+++ intermediate.js
@@ -1,20 +1,35 @@
-var grades = [76,85,65,93,81];
-// parseInt(array) = 0;
-average = 0;
-for (i=0; i<grades.length; i++){
-	average = (average + grades[i]);
-	// average = average * 1/(grades.length+1);
-	aver = average / grades.length;
+// var grades = [76,85,65,93,81];
+// average = 0;
+// for (i=0; i<grades.length; i++){
+// 	average = (average + grades[i]);
+// 	aver = average / grades.length;
+// }
+
+// document.write (average);
+// console.log (aver);
+
+// switch (true) {
+// 	case aver >=90:
+// 	console.log("A");
+// 	break;
+// 	case aver >=80:
+// 	console.log("B");
+// 	break;
+// }
+
+var n = prompt("How many numbers would you like")
+function fizzbuzz() {
+	for (i=1; i<=n; i++) {
+		if (i%15 == 0) {
+			document.write("FizzBuzz" + "<br>");
+		} else if (i%3 == 0) {
+			document.write("Fizz" + "<br>");
+		} else if (i%5 == 0) {
+			document.write("Buzz" + "<br>");
+		} else {
+			document.write(i + "<br>")
+		}
+	}
 }
 
-document.write (average);
-console.log (aver);
-
-switch (true) {
-	case aver >=90:
-	console.log("A");
-	break;
-	case aver >=80:
-	console.log("B");
-	break;
-}
+fizzbuzz();
